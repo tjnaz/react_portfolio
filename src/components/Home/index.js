f281de8af6b4a8e271202b48745593c5af249a3c
Thaj Anaz <anaz.thaj@gmail.com>
2023-07-03 23:15:00 +0530

    add: content for the home component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,24 @@
+import { Link } from "react-router-dom";
 import "./index.scss";
+import LogoTitle from "../../assets/images/logo-s.png";
 
 const Home = () => {
   return (
     <div className="container home-page">
-      <div className="text-zone"></div>
+      <div className="text-zone">
+        <h1>
+          Hi, <br /> I'm
+          <img src={LogoTitle} alt="developer" />
+          lobodan
+          <br />
+          web developer
+        </h1>
+
+        <h2>Frontent Developer / JavaScript Expert / Tweety Bird</h2>
+        <Link to="/contact" className="flat-button">
+          CONTACT ME
+        </Link>
+      </div>
     </div>
   );
 };
