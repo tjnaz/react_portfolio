6bd873db66f1698a4b9c36ce96a38b631bde33cc
Thaj Anaz <anaz.thaj@gmail.com>
2023-07-04 03:12:34 +0530

    add: use useState for animatedletter component to animate individual letters

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,15 +1,40 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import "./index.scss";
 import LogoTitle from "../../assets/images/logo-s.png";
+import AnimatedLetters from "../AnimatedLetters";
 
 const Home = () => {
+  const [letterClass, setLetterClass] = useState("text-animate");
+  const nameArray = ["l", "o", "b", "o", "d", "a", "n"];
+  const jobArray = [
+    "w",
+    "e",
+    "b",
+    " ",
+    "d",
+    "e",
+    "v",
+    "e",
+    "l",
+    "o",
+    "p",
+    "e",
+    "r",
+    ".",
+  ];
+
   return (
     <div className="container home-page">
       <div className="text-zone">
         <h1>
           Hi, <br /> I'm
           <img src={LogoTitle} alt="developer" />
-          lobodan
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray}
+            idx={15}
+          />
           <br />
           web developer
         </h1>
