61488f5941aa12def6b910b6028e00c557a5f03b
Thaj Anaz <anaz.thaj@gmail.com>
2023-07-08 14:34:31 +0530

    add: import useRef for logo reference

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,7 +1,12 @@
+import { useRef } from "react";
 import "./index.scss";
 import LogoS from "../../../assets/images/logo-s.png";
 
 const Logo = () => {
+  const bgRef = useRef();
+  const outlineLogoRef = useRef();
+  const solidLogoRef = useRef();
+
   return (
     <div className="logo-container">
       <img className="solid-logo" src={LogoS} alt="S" />
