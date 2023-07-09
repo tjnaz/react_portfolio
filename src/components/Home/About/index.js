f864005468ac42f8939b3416176db8c2017f135e
Thaj Anaz <anaz.thaj@gmail.com>
2023-07-09 13:32:14 +0530

    feat: set up the useState changes for text-animate class

--- src/components/Home/About/index.js
+++ src/components/Home/About/index.js
@@ -1,7 +1,15 @@
+import { useEffect, useState } from "react";
 import "./index.scss";
 import AnimatedLetters from "../../AnimatedLetters";
 
 const About = () => {
+  const [letterClass, setLetterClass] = useState("text-animate");
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass("text-animate-hover");
+    }, 4000);
+  }, []);
+
   return (
     <div className="container about-page">
       <div className="text-zone">
