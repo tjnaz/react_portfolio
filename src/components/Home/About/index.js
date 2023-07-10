e19c0ebdda01084b43b6bf8747a0e50199e59f29
Thaj Anaz <anaz.thaj@gmail.com>
2023-07-10 21:30:29 +0530

    add: right section of the about me

--- src/components/Home/About/index.js
+++ src/components/Home/About/index.js
@@ -1,4 +1,14 @@
 import { useEffect, useState } from "react";
+import Loader from "react-loaders";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faAngular,
+  faHtml5,
+  faCss3,
+  faReact,
+  faJsSquare,
+  faGitAlt,
+} from "@fortawesome/free-brands-svg-icons";
 import "./index.scss";
 import AnimatedLetters from "../../AnimatedLetters";
 
@@ -7,34 +17,66 @@ const About = () => {
   useEffect(() => {
     setTimeout(() => {
       setLetterClass("text-animate-hover");
-    }, 4000);
+    }, 3000);
   }, []);
 
   return (
-    <div className="container about-page">
-      <div className="text-zone">
-        <h1>
-          <AnimatedLetters
-            strArray={["A", "b", "o", "u", "t", " ", "M", "e"]}
-            idx={15}
-          />
-        </h1>
-        <p>
-          I'm a very ambitious front-end developer looking for a role in an
-          established IT company with the opportunity to work with the latest
-          technologies on challenging and diverse projects.
-        </p>
-        <p align="LEFT">
-          I'm quiet confident, naturally curious, and perpetually working on
-          improving my chops one design problem at a time.
-        </p>
-        <p>
-          If I need to define myself in one sentence that would be a family
-          person, father of a beautiful daughter, a sports fanatic, photography
-          enthusiast, and tech-obsessed!!!
-        </p>
+    <>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={["A", "b", "o", "u", "t", " ", "M", "e"]}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I'm a very ambitious front-end developer looking for a role in an
+            established IT company with the opportunity to work with the latest
+            technologies on challenging and diverse projects.
+          </p>
+          <p align="LEFT">
+            I'm quiet confident, naturally curious, and perpetually working on
+            improving my chops one design problem at a time.
+          </p>
+          <p>
+            If I need to define myself in one sentence that would be a family
+            person, father of a beautiful daughter, a sports fanatic,
+            photography enthusiast, and tech-obsessed!!!
+          </p>
+        </div>
+
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faAngular} color="#dd0031" />
+            </div>
+
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#f06529" />
+            </div>
+
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#28a8d9" />
+            </div>
+
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ed4f4" />
+            </div>
+
+            <div className="face5">
+              <FontAwesomeIcon icon={faJsSquare} color="#efd81d" />
+            </div>
+
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#ec4d28" />
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+      <Loader type="pacman" />
+    </>
   );
 };
 
