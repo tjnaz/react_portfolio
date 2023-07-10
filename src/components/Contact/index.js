213ddf5643918fee06f91a132edf3a6a1c51bc47
Thaj Anaz <anaz.thaj@gmail.com>
2023-07-11 02:57:41 +0530

    add: structural content for the contact section of the side

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -27,6 +27,41 @@ const Contact = () => {
             or large projects. However, if you have other request or questions,
             don't hesitate to contact me using the below form either.
           </p>
+          <div className="contact-form">
+            <form>
+              <ul>
+                <li className="half">
+                  <input type="text" name="name" placeholder="Name" required />
+                </li>
+                <li className="half">
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    placeholder="Subject"
+                    type="text"
+                    name="subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea
+                    placeholder="Message"
+                    name="message"
+                    required
+                  ></textarea>
+                </li>
+                <li>
+                  <input type="submit" className="flat-button" value="SEND" />
+                </li>
+              </ul>
+            </form>
+          </div>
         </div>
       </div>
       <Loader type="pacman" />
