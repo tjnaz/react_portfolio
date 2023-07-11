1ab0b8552f310ba3a2395b29ce76320205bfdecb
Thaj Anaz <anaz.thaj@gmail.com>
2023-07-12 03:03:48 +0530

    add: style to the dom to test

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -14,7 +14,9 @@ export default function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${montserrat.variable} font-mont`}>
+      <main
+        className={`${montserrat.variable} font-mont bg-light w-full min-h-screen`}
+      >
         <Component {...pageProps} />
       </main>
     </>
