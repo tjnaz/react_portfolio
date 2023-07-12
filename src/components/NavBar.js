237e36f4613a883d96e031b5bc6e67fe07923c57
Thaj Anaz <anaz.thaj@gmail.com>
2023-07-12 16:09:29 +0530

    add: structure to the navbar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,41 @@
 import React from "react";
+import Link from "next/link";
+import Logo from "./Logo";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <header className="w-full px-32 py-8 font-medium flex items-center justify-between">
+      <nav>
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/projects">Projects</Link>
+        <Link href="/articles">Articles</Link>
+      </nav>
+
+      <Logo />
+
+      <nav>
+        <Link href="/" target={"_blank"}>
+          T
+        </Link>
+        <Link href="/" target={"_blank"}>
+          T
+        </Link>
+        <Link href="/" target={"_blank"}>
+          T
+        </Link>
+        <Link href="/" target={"_blank"}>
+          T
+        </Link>
+        <Link href="/" target={"_blank"}>
+          T
+        </Link>
+        <Link href="/" target={"_blank"}>
+          T
+        </Link>
+      </nav>
+    </header>
+  );
 };
 
 export default NavBar;
