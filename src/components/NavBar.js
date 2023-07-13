0f65089abfe2761c3b2b285f314bb16574a700b0
Thaj Anaz <anaz.thaj@gmail.com>
2023-07-13 11:40:37 +0530

    add: link interactive style

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,21 @@
 import React from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import Logo from "./Logo";
 
 const CustomLink = ({ href, title, className = "" }) => {
+  const router = useRouter();
+
   return (
-    <Link href={href} className={`${className}`}>
+    <Link href={href} className={`${className} relative group`}>
       {title}
+      <span
+        className={`h-[1.5px] inline-block bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] easse duration-300 ${
+          router.asPath === href ? "w-full" : "w-0"
+        }`}
+      >
+        &nbsp;
+      </span>
     </Link>
   );
 };
@@ -14,10 +24,10 @@ const NavBar = () => {
   return (
     <header className="w-full px-32 py-8 font-medium flex items-center justify-between">
       <nav>
-        <CustomLink href="/" title="Home" />
-        <CustomLink href="/about" title="About" />
-        <CustomLink href="/projects" title="Projects" />
-        <CustomLink href="/articles" title="Articles" />
+        <CustomLink href="/" title="Home" className="mr-4" />
+        <CustomLink href="/about" title="About" className="mr-4" />
+        <CustomLink href="/projects" title="Projects" className="mr-4" />
+        <CustomLink href="/articles" title="Articles" className="mr-4" />
       </nav>
 
       <nav>
