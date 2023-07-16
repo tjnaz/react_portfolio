fa339e595b2db60010872d39263f1d8287e1303c
Thaj Anaz <anaz.thaj@gmail.com>
2023-07-16 16:21:22 +0530

    add: style for the footer layout

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,10 +5,21 @@ import Layout from "./Layout";
 const Footer = () => {
   return (
     <footer className="w-full border-t-2 border-solid border-dark font-medium text-lg">
-      <Layout className="py-8 flex items-center">
-        <span>Rightn</span>
-        <Link href="/">TJ</Link>
-        <Link href="/">Say hello b!</Link>
+      <Layout className="py-8 flex items-center justify-between">
+        <span>{new Date().getFullYear()} &copy; All Rights Reserved</span>
+        <div className="flex items-center">
+          Build with{" "}
+          <span className="text-primary text-2xl px-1">&hearts;</span>
+          &nbsp;TJ
+        </div>
+
+        <Link
+          href="mailto:anaz.thaj@gmail.com"
+          className="underline underline-offset"
+          target={"_blank"}
+        >
+          Say hello b!
+        </Link>
       </Layout>
     </footer>
   );
