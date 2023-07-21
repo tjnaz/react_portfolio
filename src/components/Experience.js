3d677b21743b952c65cfd3ae610fce3a695e4a3e
Thaj Anaz <anaz.thaj@gmail.com>
2023-07-21 14:43:35 +0530

    add: style to the experience

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,16 +1,24 @@
 import React from "react";
+import { useScroll } from "framer-motion";
 
 const Details = ({ position, company, companyLink, time, address, work }) => {
   return (
-    <li>
+    <li className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between">
       <div>
-        <h3>
-          {position}&nbsp;<a href={companyLink}>@{company}</a>
+        <h3 className="capitalize font-bold text-2xl">
+          {position}&nbsp;
+          <a
+            href={companyLink}
+            target={"_blank"}
+            className="text-primary capitalize"
+          >
+            @{company}
+          </a>
         </h3>
-        <span>
+        <span className="capitalize font-medium text-dark/75">
           {time} | {address}
         </span>
-        <p>{work}</p>
+        <p className="font-medium w-full">{work}</p>
       </div>
     </li>
   );
@@ -24,7 +32,9 @@ const Experience = () => {
       </h2>
 
       <div className="w-[75%] mx-auto relative">
-        <ul>
+        <div className="absolute left-8 top-0 w-[4px] h-full bg-dark origin-top" />
+
+        <ul className="w-full flex flex-col items-start justify-between ml-4">
           <Details
             position="Software Engineer"
             company="Google"
