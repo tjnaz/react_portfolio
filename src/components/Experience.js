e26b97feb11a5f6bb60a1d027ca1979ab74ed2e6
Thaj Anaz <anaz.thaj@gmail.com>
2023-07-21 15:45:59 +0530

    add: scrollYProgress

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { motion, useScroll } from "framer-motion";
 
 const Details = ({ position, company, companyLink, time, address, work }) => {
@@ -25,14 +25,23 @@ const Details = ({ position, company, companyLink, time, address, work }) => {
 };
 
 const Experience = () => {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start end", "center start"],
+  });
+
   return (
     <div className="my-64">
       <h2 className="font-bold text-8xl mb-32 w-full text-center">
         Experience
       </h2>
 
-      <div className="w-[75%] mx-auto relative">
-        <div className="absolute left-8 top-0 w-[4px] h-full bg-dark origin-top" />
+      <div ref={ref} className="w-[75%] mx-auto relative">
+        <motion.div
+          style={{ scaleY: scrollYProgress }}
+          className="absolute left-8 top-0 w-[4px] h-full bg-dark origin-top"
+        />
 
         <ul className="w-full flex flex-col items-start justify-between ml-4">
           <Details
