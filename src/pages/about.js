20b22c1f65129e28a3bfd32f29ee96249d8d4cd3
Thaj Anaz <anaz.thaj@gmail.com>
2023-07-21 18:26:25 +0530

    add: import education component to the about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,6 +7,7 @@ import Layout from "../components/Layout";
 import profilePic from "../../public/images/profile/developer-pic-2.jpg";
 import Skills from "../components/Skills";
 import Experience from "../components/Experience";
+import Education from "../components/Education";
 
 const AnimatedNumbers = ({ value }) => {
   const ref = useRef(null);
@@ -103,6 +104,7 @@ const about = () => {
           </div>
           <Skills />
           <Experience />
+          <Education />
         </Layout>
       </main>
     </>
