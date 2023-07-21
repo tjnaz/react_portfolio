4408d646467bd4c4175b0b07b8800d1aa1809e98
Thaj Anaz <anaz.thaj@gmail.com>
2023-07-21 15:50:35 +0530

    add: svg circle to the component

--- src/components/LiIcon.js
+++ src/components/LiIcon.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const LiIcon = () => {
-  return <div>LiIcon</div>;
+  return (
+    <figure>
+      <svg width="75" height="75" viewBox="0 0 100 100">
+        <circle cx="75" cy="50" r="20" />
+        <circle cx="75" cy="50" r="20" />
+        <circle cx="75" cy="50" r="20" />
+      </svg>
+    </figure>
+  );
 };
 
 export default LiIcon;
